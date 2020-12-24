374fc3d6e77cefbfa83a12abdfa36aeb678e1a95
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2020-12-24 13:30:14 +0200

    Added filter

--- app.js
+++ app.js
@@ -47,7 +47,11 @@ function deleteCheck(e) {
     //delete todo
     if (item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        //animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        });
     }
 
     //check mark
